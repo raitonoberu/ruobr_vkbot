e2fcef0c83243bfedc5c1c998117b813029a95aa
Denis <raitonoberu@mail.ru>
2020-09-18 23:22:08 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ from os import getenv
 DATABASE_FILE = "users.db"
 TOKEN = getenv("VK_TOKEN")
 ID = getenv("VK_ID")  # ID группы с ботом
-WAIT_IN_MINS = 20 or getenv("WAIT_IN_MINS")  # таймер обновления оценок
-TIMEZONE = "Asia/Novokuznetsk" or getenv("TIMEZONE")  # часовой пояс
+WAIT_IN_MINS = getenv("WAIT_IN_MINS") or 20  # таймер обновления оценок
+TIMEZONE = getenv("TIMEZONE") or "Asia/Novokuznetsk"  # часовой пояс
